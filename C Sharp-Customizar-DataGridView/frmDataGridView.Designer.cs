caf7906ce337197047e2e360fafbd04ef0b07913
Marco Llapapasca <marcollapapasca@gmail.com>
2020-10-25 12:01:43 -0500

    Agregar archivos de proyecto.

---
+++ C Sharp-Customizar-DataGridView/frmDataGridView.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace C_Sharp_Customizar_DataGridView
+{
+    partial class frmDataGridView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtgNorthwind = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgNorthwind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtgNorthwind
+            // 
+            this.dtgNorthwind.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgNorthwind.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dtgNorthwind.Location = new System.Drawing.Point(0, 0);
+            this.dtgNorthwind.Name = "dtgNorthwind";
+            this.dtgNorthwind.RowHeadersWidth = 62;
+            this.dtgNorthwind.RowTemplate.Height = 28;
+            this.dtgNorthwind.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dtgNorthwind.Size = new System.Drawing.Size(1035, 474);
+            this.dtgNorthwind.TabIndex = 2;
+            // 
+            // frmDataGridView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 474);
+            this.Controls.Add(this.dtgNorthwind);
+            this.Name = "frmDataGridView";
+            this.Text = "C# - Customizar DataGridView | DB Northwind";
+            this.Load += new System.EventHandler(this.frmDataGridView_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dtgNorthwind)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtgNorthwind;
+    }
+}
---
+++ C Sharp-Customizar-DataGridView/frmDataGridView.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace C_Sharp_Customizar_DataGridView
+{
+    public partial class frmDataGridView : Form
+    {
+        public frmDataGridView()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDataGridView_Load(object sender, EventArgs e)
+        {
+            /* Habilitar el cambio de tamaño en los encabezados de columna */
+            dtgNorthwind.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+
+            /* Ajustamos el alto para la columna cabecera */
+            dtgNorthwind.ColumnHeadersHeight = dtgNorthwind.ColumnHeadersHeight * 4;
+
+            /* Ajuste la alineación del texto en los encabezados de las  
+               columnas para que la visualización de texto en el centro de la parte inferior */
+            dtgNorthwind.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.BottomCenter;
+
+
+            conectar_sql();
+        }
+
+
+        private void dtgNorthwind_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            try
+            {
+                String rows = (e.RowIndex + 1).ToString();
+                SizeF size = e.Graphics.MeasureString(rows, this.Font);
+
+                if (dtgNorthwind.RowHeadersWidth < (size.Width + 20))
+                {
+                    dtgNorthwind.RowHeadersWidth = Convert.ToInt32(size.Width + 20);
+                }
+                Brush brush = SystemBrushes.ControlText;
+                e.Graphics.DrawString(rows, this.Font, brush, e.RowBounds.Location.X + 15, e.RowBounds.Location.Y + ((e.RowBounds.Height - size.Height) / 2));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "C# - Customizar DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void conectar_sql()
+        {
+            string connetionString = null;
+            SqlConnection connection;
+            SqlCommand command;
+            string sql = null;
+
+            connetionString = "Data Source=DESKTOP-06LHNBE\\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+            sql = "  " +
+                "   select cust.CustomerID, cust.ContactName,cust.ContactTitle, cust.Phone, " +
+                "    ord.OrderID, ord.OrderDate, ord.RequiredDate, ord.ShippedDate, " +
+                "    prod.ProductName, ord_d.UnitPrice, ord_d.Quantity, ord_d.Discount  " +
+                "    from dbo.Customers cust inner join dbo.Orders ord  " +
+                "    on cust.CustomerID = ord.CustomerID inner join dbo.[Order Details] ord_d  " +
+                "    on ord.OrderID = ord_d.OrderID inner join dbo.Products prod  " +
+                "    on prod.ProductID = ord_d.ProductID  " +
+                " ";
+
+            connection = new SqlConnection(connetionString);
+            try
+            {
+                connection.Open();
+                command = new SqlCommand(sql, connection);
+                //command.ExecuteNonQuery();
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable dt = new DataTable();
+                dt.Load(reader);
+
+                dtgNorthwind.DataSource = dt;
+
+
+                command.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Can not open connection ! ");
+            }
+        }
+
+        private void dtgNorthwind_Paint(object sender, PaintEventArgs e)
+        {
+            try
+            {
+                /* Datos para las celdas de cabezera */
+                String[] groupBy = { "Customers", "Orders", "Order Details" };
+
+                SolidBrush br;
+                Pen p;
+
+                for (int i = 0; i < dtgNorthwind.ColumnCount - 1; i += 4)
+                {
+                    /* Obtener los límites de encabezado de columna  */
+                    Rectangle r1 = dtgNorthwind.GetCellDisplayRectangle(i, -1, true);
+                    r1.X += 1;
+                    r1.Y += 1;
+                    r1.Width = r1.Width * 4 - 4;
+                    r1.Height = r1.Height / 2 - 2;
+
+                    br = new SolidBrush(dtgNorthwind.ColumnHeadersDefaultCellStyle.BackColor);
+                    e.Graphics.FillRectangle(br, r1);
+
+                    p = new Pen(SystemColors.InactiveBorder);
+                    e.Graphics.DrawLine(p, r1.X, r1.Bottom, r1.Right, r1.Bottom);
+
+                    StringFormat format = new StringFormat();
+
+                    br = new SolidBrush(dtgNorthwind.ColumnHeadersDefaultCellStyle.ForeColor);
+                    format.Alignment = StringAlignment.Center;
+                    format.LineAlignment = StringAlignment.Center;
+                    e.Graphics.DrawString(groupBy[i / 4], dtgNorthwind.ColumnHeadersDefaultCellStyle.Font, br, r1, format);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "C# - Customizar DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dtgNorthwind_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            try
+            {
+                if (e.RowIndex == -1 && e.ColumnIndex > -1)
+                {
+                    e.PaintBackground(e.CellBounds, false);
+                    Rectangle r2 = e.CellBounds;
+                    r2.Y += e.CellBounds.Height / 2;
+                    r2.Height = e.CellBounds.Height / 2;
+                    e.PaintContent(r2);
+                    e.Handled = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "C# - Customizar DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void dtgNorthwind_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            try
+            {
+                String ContactTitle = "";
+                String TextBackcolor = "";
+
+                /* Indica de cual columna deseo dar formato de celda*/
+                if (dtgNorthwind.Columns[e.ColumnIndex].Name.Equals("ContactTitle"))
+                {
+                    if (e.RowIndex < dtgNorthwind.RowCount - 1)
+                    {
+                        /* Captura el valor de la celda */
+                        ContactTitle = dtgNorthwind.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+                        e.CellStyle.BackColor = Color.WhiteSmoke;
+
+                        if (ContactTitle.Equals("Owner"))
+                        {
+                            e.CellStyle.ForeColor = Color.Green;
+                        }
+                        else if (ContactTitle.Equals("Accounting Manager"))
+                        {
+                            e.CellStyle.ForeColor = Color.Tomato;
+                        }
+                        else if (ContactTitle.Equals("Marketing Manager"))
+                        {
+                            e.CellStyle.ForeColor = Color.DarkCyan;
+                        }
+                        else if (ContactTitle.Equals("Sales Agent"))
+                        {
+                            e.CellStyle.ForeColor = Color.Blue;
+                        }
+                    }
+                }
+
+                if (dtgNorthwind.Columns[e.ColumnIndex].Name.Equals("UnitPrice") ||
+                    dtgNorthwind.Columns[e.ColumnIndex].Name.Equals("Quantity") ||
+                    dtgNorthwind.Columns[e.ColumnIndex].Name.Equals("Discount")
+                    )
+                {
+                    if (e.RowIndex < dtgNorthwind.RowCount - 1)
+                    {
+
+                        // Captura el valor de la celda 
+                        TextBackcolor = dtgNorthwind.Rows[e.RowIndex].Cells[e.ColumnIndex].Value.ToString();
+
+                        if (TextBackcolor.Equals("0"))
+                        {
+                            e.CellStyle.BackColor = Color.MistyRose;
+                        }
+                        else
+                        {
+                            e.CellStyle.BackColor = Color.LemonChiffon;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "C# - Customizar DataGridView", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
